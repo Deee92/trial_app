247acac57e377b3deaa7008a8dd863f737d27db5
Deepika Tiwari <deepikatiwari92@gmail.com>
2015-03-30 16:09:59 +0530

    Provide titles for home, listing, about
    Use application helper.
    Related tests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Provides title to each page.
+	def full_title(page_title = '')
+		base_title = "Divyavani"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Divyavani"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
     assert_template 'static_pages/home'
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get listing" do
     get :listing
     assert_response :success
     assert_template 'static_pages/listing'
+    assert_select "title", "Listing | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
     assert_template 'static_pages/about'
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
